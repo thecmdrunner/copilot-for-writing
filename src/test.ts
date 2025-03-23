6498708a7051ebd6ff1096a8a75df48c1e5d172f
Pranav <38887390+thecmdrunner@users.noreply.github.com>
2025-03-23 19:45:31 +0530

    test

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import { readFileSync } from "fs";
+config({ path: ".env.prod" });
+
+console.log(`GITHUB ENVS`, {
+  GITHUB_SHA: process.env.GITHUB_SHA,
+  GITHUB_REF: process.env.GITHUB_REF,
+  GITHUB_REF_NAME: process.env.GITHUB_REF_NAME,
+  GITHUB_REF_TYPE: process.env.GITHUB_REF_TYPE,
+});
+
+const envFile = readFileSync(".env.prod", "utf-8");
+console.log(envFile);
